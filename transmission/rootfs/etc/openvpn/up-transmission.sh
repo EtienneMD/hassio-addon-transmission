c374d25835d2a9af829ffe864fe50fb19e465867
EtienneMD <etienne.md@me.com>
2021-03-07 23:22:50 -0500

    Update up-transmission.sh

--- transmission/rootfs/etc/openvpn/up-transmission.sh
+++ transmission/rootfs/etc/openvpn/up-transmission.sh
@@ -2,10 +2,10 @@
 # ==============================================================================
 declare CONFIG
 
-CONFIG=$(</etc/transmission/settings.json)
+CONFIG=$(</data/transmission/settings.json)
 
 CONFIG=$(bashio::jq "${CONFIG}" ".\"bind-address-ipv4\"=\"${1}\"")
 
 echo "${CONFIG}" > /data/transmission/settings.json
 
-exec /usr/bin/transmission-daemon --foreground --config-dir /etc/transmission
+exec /usr/bin/transmission-daemon --foreground --config-dir /data/transmission
