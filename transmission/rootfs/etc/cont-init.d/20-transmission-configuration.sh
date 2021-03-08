a8840a2fb3b68fdfb7dcefc207fd05378a65655a
EtienneMD <etienne.md@me.com>
2021-03-07 21:17:59 -0500

    Customize settings

--- transmission/rootfs/etc/cont-init.d/20-transmission-configuration.sh
+++ transmission/rootfs/etc/cont-init.d/20-transmission-configuration.sh
@@ -17,12 +17,26 @@ fi
 CONFIG=$(</data/transmission/settings.json)
 
 # Defaults
-CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir\"=\"/media/incomplete\"")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=false")
-CONFIG=$(bashio::jq "${CONFIG}" ".\"download-dir\"=\"/media/downloads\"")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"download-dir\"=\"/media/Seagate/Downloads\"")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-host-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"bind-address-ipv4\"=\"0.0.0.0\"")
+
+# Speed limits
+CONFIG=$(bashio::jq "${CONFIG}" ".\"speed-limit-down-enabled\"=true")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"speed-limit-down\"=800")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"speed-limit-up-enabled\"=true")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"speed-limit-up\"=1")
+
+# Alt speeds
+CONFIG=$(bashio::jq "${CONFIG}" ".\"alt-speed-down\"=200")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"alt-speed-up\"=1")
+
+# Ratio limits
+CONFIG=$(bashio::jq "${CONFIG}" ".\"ratio-limit-enabled\"=true")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"ratio-limit\"=0")
+
 
 authentication_required=$(bashio::config 'authentication_required')
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-authentication-required\"=${authentication_required}")
